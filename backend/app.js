5fb5ce8de29abf43ff4c0184b476de791e2e6783
Oussama Amou <amoussdark@gmail.com>
2025-12-11 15:57:08 +0100

    refactor: intégration des routes et middlewares d'authentification dans l'app

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,11 @@
 import express from "express";
 import { errorHandler } from "./src/middlewares/errorHandler.js";
+import authRoutes from "./src/routes/authRoutes.js";
 
 const app = express();
 app.use(express.json());
 
-app.get('/test-error', (req, res, next) => {
-
-    const error = new Error("Ceci est un test d'erreur");
-    res.statusCode = 400; 
-    next(error); 
-});
+app.use('/api', authRoutes);
 
 app.use(errorHandler);
 
