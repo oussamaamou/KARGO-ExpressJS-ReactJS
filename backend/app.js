557e57c7584b992d398a96aef352bec7daa8a67c
Oussama Amou <amoussdark@gmail.com>
2025-12-12 15:16:38 +0100

    refactor: intégration des nouvelles routes camion dans l'application

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from 'cors';
 import { errorHandler } from "./src/middlewares/errorHandler.js";
 import authRoutes from "./src/routes/authRoutes.js";
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 app.use('/api', authRoutes);
 
