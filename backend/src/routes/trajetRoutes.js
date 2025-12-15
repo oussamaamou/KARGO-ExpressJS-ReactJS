042b70cac2aaf05969cecd7a5bea1c23e54ad466
Oussama Amou <amoussdark@gmail.com>
2025-12-15 20:04:45 +0100

    feat: configuration des routes pour la gestion des trajets

---
+++ backend/src/routes/trajetRoutes.js
@@ -0,0 +1,15 @@
+import express from "express";
+import TrajetController from "../controllers/TrajetController.js";
+import authMiddleware from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.use(authMiddleware.protect);
+
+router.route("/trajets")
+    .get(authMiddleware.authorize("admin"), TrajetController.getAllTrajets)
+    .post(authMiddleware.authorize("admin"), TrajetController.createTrajet);
+
+router.get("/mes-trajets", TrajetController.getMyTrajets);
+
+export default router;
