2ae2efbf00a01f637107facdf7818f75966c0006
Oussama Amou <amoussdark@gmail.com>
2025-12-17 15:08:58 +0100

    refactor: intégration des routes dashboard dans l'application

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,10 @@ import cors from 'cors';
 import { errorHandler } from "./src/middlewares/errorHandler.js";
 import authRoutes from "./src/routes/authRoutes.js";
 import camionRoutes from "./src/routes/camionRoutes.js";
-import remoqueRoutes from "./src/routes/remoqueRoutes.js";
+import remoqueRouter from "./src/routes/remoqueRoutes.js";
 import maintenanceRoutes from './src/routes/maintenanceRoutes.js';
 import trajetRoutes from './src/routes/trajetRoutes.js';
+import dashboardRouter from "./src/routes/dashboardRoutes.js";
 
 const app = express();
 app.use(express.json());
@@ -13,9 +14,10 @@ app.use(cors());
 
 app.use('/api', authRoutes);
 app.use('/api', camionRoutes);
-app.use('/api', remoqueRoutes);
+app.use('/api', remoqueRouter);
 app.use('/api/maintenance', maintenanceRoutes);
 app.use('/api', trajetRoutes);
+app.use('/api', dashboardRouter)
 
 app.use(errorHandler);
 
