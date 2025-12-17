6cd88bf885e7f3d85ba0722b607f11b0e8e8b559
Oussama Amou <amoussdark@gmail.com>
2025-12-17 21:48:55 +0100

    refactor: amélioration de la navigation du layout chauffeur

--- frontend/src/layouts/ChauffeurLayout.jsx
+++ frontend/src/layouts/ChauffeurLayout.jsx
@@ -1,4 +1,4 @@
-import { Outlet, useNavigate } from 'react-router-dom';
+import { Outlet, useNavigate, NavLink } from 'react-router-dom';
 import { useAuth } from '../context/authContext';
 
 const ChauffeurLayout = () => {
@@ -26,7 +26,8 @@ const ChauffeurLayout = () => {
                     <path d="M3 10C3 10.5523 3.44772 11 4 11L12 11C12.5523 11 13 10.5523 13 10V4C13 3.44772 12.5523 3 12 3H4C3.44772 3 3 3.44772 3 4V10ZM11 20C11 20.5523 11.4477 21 12 21H20C20.5523 21 21 20.5523 21 20V14C21 13.4477 20.5523 13 20 13H12C11.4477 13 11 13.4477 11 14V20ZM13 15H19V19H13V15ZM3 20C3 20.5523 3.44772 21 4 21H8C8.55229 21 9 20.5523 9 20V14C9 13.4477 8.55229 13 8 13H4C3.44772 13 3 13.4477 3 14V20ZM5 19V15H7V19H5ZM5 9V5L11 5L11 9L5 9ZM20 11C20.5523 11 21 10.5523 21 10V4C21 3.44772 20.5523 3 20 3H16C15.4477 3 15 3.44772 15 4V10C15 10.5523 15.4477 11 16 11H20ZM19 9H17V5H19V9Z">
                     </path>
                 </svg>,
-                            label: 'Tableau de bord', active: true
+                            label: 'Tableau de bord', path: "dashboard"
+
                         },
                         {
                             icon: 
@@ -34,27 +35,23 @@ const ChauffeurLayout = () => {
                     <path d="M3 10C3 10.5523 3.44772 11 4 11L12 11C12.5523 11 13 10.5523 13 10V4C13 3.44772 12.5523 3 12 3H4C3.44772 3 3 3.44772 3 4V10ZM11 20C11 20.5523 11.4477 21 12 21H20C20.5523 21 21 20.5523 21 20V14C21 13.4477 20.5523 13 20 13H12C11.4477 13 11 13.4477 11 14V20ZM13 15H19V19H13V15ZM3 20C3 20.5523 3.44772 21 4 21H8C8.55229 21 9 20.5523 9 20V14C9 13.4477 8.55229 13 8 13H4C3.44772 13 3 13.4477 3 14V20ZM5 19V15H7V19H5ZM5 9V5L11 5L11 9L5 9ZM20 11C20.5523 11 21 10.5523 21 10V4C21 3.44772 20.5523 3 20 3H16C15.4477 3 15 3.44772 15 4V10C15 10.5523 15.4477 11 16 11H20ZM19 9H17V5H19V9Z">
                     </path>
                 </svg>,
-                            label: 'Investissements', active: false
-                        },
-                        {
-                            icon: 
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="currentColor">
-                    <path d="M3 10C3 10.5523 3.44772 11 4 11L12 11C12.5523 11 13 10.5523 13 10V4C13 3.44772 12.5523 3 12 3H4C3.44772 3 3 3.44772 3 4V10ZM11 20C11 20.5523 11.4477 21 12 21H20C20.5523 21 21 20.5523 21 20V14C21 13.4477 20.5523 13 20 13H12C11.4477 13 11 13.4477 11 14V20ZM13 15H19V19H13V15ZM3 20C3 20.5523 3.44772 21 4 21H8C8.55229 21 9 20.5523 9 20V14C9 13.4477 8.55229 13 8 13H4C3.44772 13 3 13.4477 3 14V20ZM5 19V15H7V19H5ZM5 9V5L11 5L11 9L5 9ZM20 11C20.5523 11 21 10.5523 21 10V4C21 3.44772 20.5523 3 20 3H16C15.4477 3 15 3.44772 15 4V10C15 10.5523 15.4477 11 16 11H20ZM19 9H17V5H19V9Z">
-                    </path>
-                </svg>,
-                            label: 'Statistiques', active: false
+                            label: 'Mes Trajets', path: "mes-trajets"
                         },
                     ].map((item) => (
-                        <button
-                            key={item.label}
-                            className={`w-full flex items-center gap-4 p-3 rounded-lg transition-colors ${item.active
-                                ? 'text-white hover:bg-indigo-800/50 hover:text-white'
-                                : 'text-white hover:bg-indigo-800/50 hover:text-white'
-                                }`}
+                        <NavLink
+                            key={item.path}
+                            to={item.path}
+                            className={({ isActive }) => 
+                                `w-full flex items-center gap-4 p-3 rounded-lg transition-colors ${
+                                    isActive
+                                    ? 'bg-indigo-950 text-white shadow-md' // Style quand on est sur la page (Actif)
+                                    : 'text-indigo-100 hover:bg-indigo-800/50 hover:text-white' // Style par défaut
+                                }`
+                            }
                         >
                             <span>{item.icon}</span>
                             <span className="hidden lg:block font-medium text-sm">{item.label}</span>
-                        </button>
+                        </NavLink>
                     ))}
                 </nav>
 
